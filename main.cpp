a3bc0e0d183544d8d56a319ddcf2c39993ba387d
Andrés Pérez <Oxyrus@users.noreply.github.com>
2017-08-21 15:12:13 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,7 +5,6 @@
 constexpr int windowWidth{800}, windowHeight{600};
 constexpr float ballRadius{10.f}, ballVelocity{8.f};
 constexpr float paddleWidth{60.f}, paddleHeight{20.f}, paddleVelocity{6.f};
-
 constexpr float blockWidth{60.f}, blockHeight{20.f};
 constexpr int countBlocksX{11}, countBlocksY{4};
 
